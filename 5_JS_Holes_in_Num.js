95a63bf58044a43f0b0e5738e0ca6bea7fd322ab
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-11-22 21:42:59 +0100

    holes in number

---
+++ 5_JS_Holes_in_Num.js
@@ -0,0 +1,23 @@
+function closedPaths(number) {
+  // Write your code here
+
+  let paths = 0;
+
+  const numStr = number.toString();
+
+  for (let i = 0; i < numStr.length; i++) {
+    if (numStr[i] === "8") {
+      paths += 2;
+    } else if (
+      numStr[i] === "0" ||
+      numStr[i] === "4" ||
+      numStr[i] === "6" ||
+      numStr[i] === "9"
+    ) {
+      paths += 1;
+    } else paths;
+  }
+  return paths;
+}
+
+console.log(closedPaths(630));
