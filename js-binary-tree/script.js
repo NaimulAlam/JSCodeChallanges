2ef5a6b53585112cb55ec9f6d74ae523b57c3fc2
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-11-26 05:33:09 +0100

    vanila JS Binary Tree completed with some better design

--- js-binary-tree/script.js
+++ js-binary-tree/script.js
@@ -2,23 +2,66 @@ const getTreeData = () => {
   return {
     element: "test",
     left: {
-      element: "test12",
+      element: "test01",
       left: {
-        element: "test122",
+        element: "test11",
       },
       right: {
-        element: "test122",
+        element: "test12",
       },
     },
     right: {
-      element: "test123",
+      element: "test02",
+      left: {
+        element: "test21",
+      },
       right: {
-        element: "test122",
+        element: "test22",
+        left: {
+          element: "test221",
+          left: {
+            element: "test2211",
+          },
+        },
       },
     },
   };
 };
 
-const main = () => {};
+const renderTree = (node) => {
+  const { left, right, element } = node;
+  return `
+        <div class="node_element">${element}</div>
+          ${
+            left || right
+              ? `
+    <div class="node_bottom-line"></div>
+            <div class="node_children">
+              ${
+                left
+                  ? `<div class="node node-left">
+                ${renderTree(left)}
+              </div>`
+                  : ""
+              }
+              ${
+                right
+                  ? `<div class="node node-right">
+                ${renderTree(right)}
+              </div>`
+                  : ""
+              }
+            </div>
+            `
+              : ""
+          }
+          `;
+};
+
+const main = () => {
+  const rootNode = getTreeData();
+  const treeDOMElement = document.querySelector(".tree");
+  treeDOMElement.innerHTML = renderTree(rootNode);
+};
 
 main();
