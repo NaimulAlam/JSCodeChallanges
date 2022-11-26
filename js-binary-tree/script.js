d34cae5f356639ce1565fc1d58ff674fc0cbe5a2
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-11-26 02:57:05 +0100

    vanila JS Binary Tree visual setup and .js ext missing added to no-2

---
+++ js-binary-tree/script.js
@@ -0,0 +1,24 @@
+const getTreeData = () => {
+  return {
+    element: "test",
+    left: {
+      element: "test12",
+      left: {
+        element: "test122",
+      },
+      right: {
+        element: "test122",
+      },
+    },
+    right: {
+      element: "test123",
+      right: {
+        element: "test122",
+      },
+    },
+  };
+};
+
+const main = () => {};
+
+main();
